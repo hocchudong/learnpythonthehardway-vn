3c03f467a33f6d94c268dc0d3bac0730cf7b85bd
Truong Thanh Ha <thienhabka@gmail.com>
2014-12-08 18:36:11 +0700

    Update ex11.py

--- Exercise-11-Asking-Question/ex11.py
+++ Exercise-11-Asking-Question/ex11.py
@@ -1,9 +1,9 @@
-print ("How old are you?")
+print "How old are you?"
 age = int(input())
-print ("How tall are you?")
+print "How tall are you?"
 height = input()
-print ("How much do you weigh?")
+print "How much do you weigh?"
 weight = input()
 
-print ("So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight))
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
