19972c27a96999b2905df5e670e57b2a7e2491e5
Truong Thanh Ha <thienhabka@gmail.com>
2014-12-08 18:36:54 +0700

    Update ex12.py

--- Exercie-12-Prompting-People/ex12.py
+++ Exercie-12-Prompting-People/ex12.py
@@ -1,6 +1,6 @@
-age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weigh? ")
+age = input "How old are you? "
+height = input "How tall are you? " 
+weight = input "How much do you weigh? "
 
-print ("So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight))
+print  "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
