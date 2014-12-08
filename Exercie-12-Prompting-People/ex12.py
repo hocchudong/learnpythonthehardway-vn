ef35298f7a6488b4f26a41dbbdce5d133388e2aa
Truong Thanh Ha <thienhabka@gmail.com>
2014-12-08 13:50:00 +0700

    Create ex12.py

---
+++ Exercie-12-Prompting-People/ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print ("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight))
