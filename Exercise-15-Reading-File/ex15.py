ef7af620e3e3459632409d510f508013ee28f658
Truong Thanh Ha <thienhabka@gmail.com>
2014-12-08 15:12:14 +0700

    Create ex15.py

---
+++ Exercise-15-Reading-File/ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print "Here's your file %r:" % filename
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input("> ")
+
+txt_again = open(file_again)
+
+print txt_again.read()
