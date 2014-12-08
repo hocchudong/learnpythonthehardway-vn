653dce61602e76b431621be386795c93df33bf68
daokhactuan48 <khactuank55@yahoo.com>
2014-12-08 18:17:22 +0700

    Phiên bản 1

---
+++ Exercise_1_A-Good-First-Program/excercise1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# In ra màn hình dòng Hello world 
+print "Hello world!"
+# In ra màn hình sử dụng dấu nhắp đơn ''
+print 'Hello world again'
+# In ra chuối mà trong đó có dấu nhắc đôi ""
+print 'Hello "world" again'
+# In ra màn hình trong đó có dấu nhắc đơn ''
+print "Hello 'world' again"
