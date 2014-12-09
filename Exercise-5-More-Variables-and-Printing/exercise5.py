d4c3bbdb9f3b78585c453ec2196fe9657cf7e27b
daokhactuan48 <khactuank55@yahoo.com>
2014-12-09 18:27:11 +0700

    Phiên bản 1

---
+++ Exercise-5-More-Variables-and-Printing/exercise5.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+my_age, my_height, my_weight, my_age + my_height + my_weight)
