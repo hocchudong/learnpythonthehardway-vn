9c0d146d2afee5a2c7241ce1876feb37f2858083
daokhactuan48 <khactuank55@yahoo.com>
2014-12-24 14:24:59 +0700

    Create exercise17.py

---
+++ Exercise-17-More-Files/exercise17.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
