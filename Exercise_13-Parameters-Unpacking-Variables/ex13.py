d78cd06acc4db27a58483a9d28a67eff9433add8
Tô Thành Công <tcvn1985@gmail.com>
2015-01-03 10:26:11 +0700

    cap nhat bai 13

--- Exercise_13-Parameters-Unpacking-Variables/ex13.py
+++ Exercise_13-Parameters-Unpacking-Variables/ex13.py
@@ -1,3 +1,8 @@
+#!/bin/python
+# Cach thu hien chuong trinh duoi
+# python ex13.py To Thanh Cong
+################################
+
 from sys import argv
 
 script, first, second, third = argv
@@ -6,3 +11,12 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+print 
+
+# Cach duoi cung la mot ky thuat hien thi
+
+print "Ten chuong trinh la: %s" %script
+print "Doi so thu nhat la: %s" %first
+print "Doi so thu hai la: %s" %second
+print "Doi so thu ba la: %s" %third
